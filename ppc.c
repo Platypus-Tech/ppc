7e5cf537a0ae9532864d2dbecc653d9e3f15f76c
GNUOverYooouu <dtortoise99@gmail.com>
2021-05-24 11:38:46 -0700

    Documented code

--- ppc.c
+++ ppc.c
@@ -5,12 +5,12 @@
 #include <stdio.h>
 
 int main(int argc, char argv){
-    if(argc < 1){
+    if(argc < 1){ // if there are no arguments
         printf("Error: no arguments");
-        return(1);
+        return(1); // Error
     }
     else if(argc <= 1){
-        if(argv == "help"){
+        if(argv == "help"){ // If you need help
             printf("Welcome to the Pigeon Potato Compiler\n");
             printf("It's in alpha stage, so really, there is no way to compile anything...\tyet.\n");
             printf("You can try though, so here's the syntax to not compile anything (if ppc is in usr/bin):\n\n");
@@ -18,9 +18,9 @@ int main(int argc, char argv){
             printf("If ppc is in your current directory, you just add ./ to the beginning, like this:\n\n");
             printf("./ppc <C source file>\n\n");
             printf("NOTE: NO SPACES IN FILENAMES.");
-            return(0);
+            return(0); // It's good
         }
-        else{
+        else{ // something... else. Maybe you said ppc astro-zombies
             printf("IG you're trying to compile a file, but currently, there's no way to compile anything :(");
             return(0);
         }
