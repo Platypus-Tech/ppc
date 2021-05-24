b4f79f5b4036b5acfefa6d91370769016bbd6343
GNUOverYooouu <dtortoise99@gmail.com>
2021-05-24 09:57:05 -0700

    Added help option

--- ppc.c
+++ ppc.c
@@ -10,7 +10,19 @@ int main(int argc, char argv){
         return(1);
     }
     else if(argc <= 1){
-        printf("IG you're trying to compile a file, but currently, there's no way to compile anything :(");
-        return(0);
+        if(argv == "help"){
+            printf("Welcome to the Pigeon Potato Compiler\n");
+            printf("It's in alpha stage, so really, there is no way to compile anything...\tyet.\n");
+            printf("You can try though, so here's the syntax to not compile anything (if ppc is in usr/bin):\n\n");
+            printf("ppc <C source file>\n\n");
+            printf("If ppc is in your current directory, you just add ./ to the beginning, like this:\n\n");
+            printf("./ppc <C source file>\n\n");
+            printf("NOTE: NO SPACES IN FILENAMES.");
+            return(0);
+        }
+        else{
+            printf("IG you're trying to compile a file, but currently, there's no way to compile anything :(");
+            return(0);
+        }
     }
 }
