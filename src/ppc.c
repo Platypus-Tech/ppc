40609448164b9deae7cb3e1bd15242688a1333d1
GNUOverYooouu <dtortoise99@gmail.com>
2021-05-25 14:29:32 -0700

    Is it gonna work now?

--- src/ppc.c
+++ src/ppc.c
@@ -42,6 +42,7 @@ int main(int argc, char *argv[]){
             /*printf("IG you're trying to compile a file, but currently, there's no way to compile anything :(\n");
             printf("If you want to read a few Misfits lyrics, try ppc astro-zombies.");*/
             FILE* f2c = argv[1]; // f2c means file to compile
+            fopen(f2c, "r")
             return(0);
         }
     }
